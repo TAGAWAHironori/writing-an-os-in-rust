4402252988005ce27250d8a092844efef6ee7efd
TAGAWAHironori <tgwh3729@gmail.com>
2022-10-01 23:56:43 +0900

    Rustバイナリ

--- blog_os/src/main.rs
+++ blog_os/src/main.rs
@@ -1,10 +1,17 @@
+//標準ライブラリの無効(panic_handlerとlanguage itemに変わるものが必要)
 #![no_std]
+//通常のエントリポイントを使用しないことを宣言
+#![no_main]
 
 use core::panic::PanicInfo;
 
-fn main() {
+//関数名をマングルしないように指示
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop{}
 }
 
+//パニックハンドラーの実装
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
